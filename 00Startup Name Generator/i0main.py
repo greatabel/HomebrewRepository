593450768561c2e4c3ed9427dd4efdf88b3ead5e
greatabel <myreceiver2for2github@gmail.com>
2018-02-12 08:26:34 +0800

    i0main

---
+++ 00Startup Name Generator/i0main.py
@@ -0,0 +1,15 @@
+from termcolor import colored
+
+# http://techcrunch.cn/2018/02/11/startups-are-still-making-weird-name-choices/
+
+
+def show(s, color='green'):
+    return colored(s, color, attrs=['reverse', 'blink'])
+
+
+def main():
+    ""
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # HomebrewRepository
 It's very important to have a feedback loop, where you're constantly thinking about what you've done and how you could be doing it better.
+平时看到、读到、想到的一些事情，然后结合自己的思考，做的一些对未来的想象的集合。
 
 
