ddd069b2e65bd3359493158a7ece6edd3b5d7ed3
greatabel <myreceiver2for2github@gmail.com>
2018-02-13 17:22:38 +0800

    5 movements

---
+++ 02WuXing/i0main.py
@@ -0,0 +1,9 @@
+import i1config as config
+
+
+def main():
+    print(config.WuXing['Wood'])
+
+
+if __name__ == '__main__':
+    main()
---
+++ 02WuXing/i1config.py
@@ -0,0 +1,7 @@
+WuXing = dict(
+    Wood = '木',
+    Fire = '火',
+    Earth = '土',
+    Metal = '金',
+    Water = '水',
+)
